4323a0aecdee971bed84828fa713e89279051d9d
sheenawangumo <sheenawangumo.contact@gmail.com>
2026-04-14 17:46:11 +0300

    improved

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
 function calculateTax(amount) {
- 
-  return amount * 0.10;
+  const taxValue = 0.1 * amount;
+  return taxValue;
 }
-function convertToUpperCase(text) {
-  
-  return text.toUpperCase();
-}
-function findMaximum(num1, num2) {
- 
-  return Math.max(num1, num2);
-}
+calculateTax(100);
 
+function convertToUpperCase(text) {
+  const upperCaseText = text.toUpperCase();
+  return upperCaseText;
+}
+convertToUpperCase("hello world");
+
+function findMaximum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {    return num2;
+  }
+}
+findMaximum(5, 10);
 
 function isPalindrome(word) {
-  
   let lowerCaseWord = word.toLowerCase();
-  
- 
-  let reversedWord = lowerCaseWord.split('').reverse().join('');
-  
-
+  let reversedWord = lowerCaseWord.split("").reverse().join("");
   return lowerCaseWord === reversedWord;
 }
-function calculateDiscountedPrice(originalPrice, discountPercentage) {
+isPalindrome("racecar");
 
-  let discountAmount = originalPrice * (discountPercentage / 100);
-  
-
-  return originalPrice - discountAmount;
+function calculateDiscountPrice(originalPrice, discountPercentage) {
+  const discountAmount = (discountPercentage / 100) * originalPrice;
+  const discountedPrice = originalPrice - discountAmount;
+  return discountedPrice;
 }
-
-
-
-
-
-
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+calculateDiscountPrice(100, 20);
